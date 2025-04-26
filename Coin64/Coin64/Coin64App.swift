5b4c2642be5a21795f4a18def4ae815490dd436f
Reza <rizamili3@gmail.com>
2025-04-26 22:36:45 +0200

    SwiftUI configuration

---
+++ Coin64/Coin64/Coin64App.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  Coin64
+//
+//  Created by Reza on 21.04.25.
+//
+
+import SwiftUI
+
+@main
+struct Coin64App: App {
+    var body: some Scene {
+        WindowGroup {
+            Text("Coin64")
+        }
+    }
+}
+
